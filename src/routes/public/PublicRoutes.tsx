ffd836b0ceba4d316ce657ed9e6ee667a307ae37
shubham choudhary <shubhchoudhary0156@gmail.com>
2026-05-26 16:07:03 +0530

    add code

--- src/routes/public/PublicRoutes.tsx
+++ src/routes/public/PublicRoutes.tsx
@@ -1,19 +1,36 @@
-
-
 import React, { Suspense } from 'react'
-import { useRoutes } from 'react-router-dom'
+import { Navigate, useRoutes } from 'react-router-dom'
+
 import BazarsLayout from '../../features/bazars/pages/bazars-layout/BazarsLayout'
 import RegularBazars from '../../features/bazars/pages/regular-bazars/RegularBazars'
 
 const PublicRoutes = () => {
+
   const routes = useRoutes([
-    { path: '/', element: <BazarsLayout />,children:[
-      {path:'regular',element:<RegularBazars/>}
-    ]}
+    {
+      path: '/',
+      element: <BazarsLayout />,
+      children: [
+
+        // default redirect
+        {
+          index: true,
+          element: <Navigate to="/regular" replace />
+        },
+
+        {
+          path: 'regular',
+          element: <RegularBazars />
+        }
+      ]
+    }
   ])
-  return <>
-  <Suspense fallback={<div>loading</div>}>{routes}</Suspense>
-  </>
+
+  return (
+    <Suspense fallback={<div>loading</div>}>
+      {routes}
+    </Suspense>
+  )
 }
 
 export default PublicRoutes
